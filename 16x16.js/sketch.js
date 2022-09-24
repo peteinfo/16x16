2bf992287cb7f982ffc8ab3fac936cc995563eca
jens <jens@lea.io>
2022-09-24 12:43:05 +0100

    Fixed RandomAccess mode mathcing improvements

--- 16x16.js/sketch.js
+++ 16x16.js/sketch.js
@@ -23,8 +23,8 @@ function setup() {
   // Note: Not needed, `grid` is already initialised; TODO: this chould chnage to user initialisation.
   // setupGrid(16, 16)
 
-  useMode("Long Sequence")
-  //useMode("Random Access")
+  // useMode("Long Sequence")
+  useMode("Random Access")
   //useMode("Prompt Mode")
   //useMode("Reflect Mode")
   //useMode("Just Write")
