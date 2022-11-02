ea894385dcdb1f999392a433f7d8ec4fe71a47a1
Dave Webb <dave.webb@cranbim.com>
2022-11-02 10:39:32 +0000

    Added dave's level 13 and 14

--- 16x16.js/modes/14-radials.js
+++ 16x16.js/modes/14-radials.js
@@ -1,4 +1,5 @@
 defineMode("14-blank", grid => {
+  let arcRenderer
 
   let timer // for being able to cancel the setTimeout call on exit
   let samples
@@ -40,6 +41,89 @@ defineMode("14-blank", grid => {
     /* 34 - y */ "./samples/nothing/0",
     /* 35 - z */ "./samples/nothing/0"
   ]
+  
+  // const cols=[[200,30,30],
+  //           [200,200,30],
+  //           [30,200,200],
+  //           [30,30,200],
+  //           [130,200,130],
+  //           [30,30,200],          
+  //         ]
+
+  const cols=[
+    [201, 138, 2], //#c98a02
+    [45, 100, 150], //#2d6496
+    [197, 60, 66], //#C53C42
+    [96, 122, 7], //#607a07
+    [70, 54, 117], //#463675
+    [158, 40, 91],  //#9e285b        
+  ]
+
+  const arcTypes={
+    a:{rIn:0.0,rOut:0.3,col:cols[0]},
+    b:{rIn:0.1,rOut:0.7,col:cols[1]},
+    c:{rIn:0.2,rOut:0.5,col:cols[2]},
+    d:{rIn:0.3,rOut:0.7,col:cols[3]},
+    e:{rIn:0.4,rOut:0.6,col:cols[4]},
+    f:{rIn:0.9,rOut:0.3,col:cols[5]},
+    g:{rIn:0.5,rOut:0.9,col:cols[0]},
+    h:{rIn:0.9,rOut:0.7,col:cols[1]},
+    i:{rIn:0.5,rOut:0.9,col:cols[2]},
+    j:{rIn:0.7,rOut:0.2,col:cols[3]},
+    k:{rIn:0.6,rOut:0.4,col:cols[4]},
+    l:{rIn:0.7,rOut:0.2,col:cols[5]},
+  }
+  class ArcRenderer{
+    constructor(){
+      this.arcs=[]
+    }
+    
+    add(a,ri,ro,col){
+      this.arcs.push(new ArcSpread(unitOf(8), unitOf(8),a,ri,ro,col));
+    }
+    
+    run(){
+      blendMode(ADD)
+      for(var i=this.arcs.length-1; i>=0; i--){
+        if(this.arcs[i].run()){
+          this.arcs[i].show();
+        } else {
+          this.arcs.splice(i,1);
+        }
+      }
+      blendMode(BLEND)
+    };
+  }
+
+  function ArcSpread(x,y,a,ri,ro,col){
+    var ttlMax=200;
+    var ttl=ttlMax;
+    var rt=ro-ri;
+    var re=ro-rt;
+    var as=0;
+    var aGrow=0.01;
+    // var colHue=frameCount%360;
+    // var colSat=80;
+    // var colBri=80;
+    
+    this.run=function(){
+      ri+=(ro-ri)/100;
+      rt=abs(ro*width/2-ri*width/2);
+      re=ri*width/2+rt/4;
+      as+=aGrow;
+      ttl--;
+      return true;
+    };
+    
+    this.show=function(){
+      strokeWeight(rt);
+      stroke(col[0],col[1],col[2],150*ttl/ttlMax);
+      strokeCap(SQUARE);
+      noFill();
+      arc(x,y,re*2,re*2,a-as, a+as);
+      noStroke()
+    }
+  }
 
   class Playhead {
     constructor(min, max, speed) {
@@ -52,49 +136,34 @@ defineMode("14-blank", grid => {
     }
   }
 
-  let playhead = new Playhead(0, 15, 200)
+  let playhead = new Playhead(0, 15, 500)
 
   function tick() {
     // this function is triggered every interval
-    playhead.pos++;
-    if (playhead.pos > playhead.max) {
-      playhead.pos = playhead.min
-    }
+    playhead.pos = (playhead.pos + 16) % 256
 
     // does the index contain a note to play?
-    if (grid.sequence[playhead.pos] != '.') {
-
-      // Great! Let's play a note
-      //print("PLAY NOTE! index: " + playhead.pos + " contains: " + grid.sequence[playhead.pos])
-      let sampleToPlay = '0'
-
-      // Small fix to avoid out of bounds
-      if (grid.sequence[playhead.pos] && grid.sequence[playhead.pos].match(/^[0-9]$/)) {
-        sampleToPlay = grid.sequence[playhead.pos]
+    
+    for(let i=0; i<16; i++){
+      if (grid.sequence[playhead.pos+i] != '.') {
+        let arcType=arcTypes[grid.sequence[playhead.pos+i]]
+        let a=(0.5+i-8)*PI/8-PI/2
+        arcRenderer.add(a,arcType.rIn,arcType.rOut,arcType.col)
       }
-      else if (grid.sequence[playhead.pos].match(/^[a-z]$/)) {
-        // convert from ascii
-        // as a is 97 in ascii, subtract 87 to shift to 10
-        sampleToPlay = grid.sequence[playhead.pos].charCodeAt(0) - 87
-      }
-
-      //samples[sampleToPlay].rate(1)
-      //samples[sampleToPlay].stop()
-      samples[sampleToPlay].volume(random(0.5, 0.9))   // 'humanise' or vary the volume slightly
-      samples[sampleToPlay].play()
     }
+
     timer = setTimeout(tick, playhead.interval)
   }
 
   return {
 
     title:
-      "\nLEVEL 14: UNDER CONSTRUCTION\
+      "\nLEVEL 14: RADIALS\
        --------------------------- \
-      New mode coming soon!",
+      a-l triggers a visual arc that fades",
 
       info:
-      "\n[1-9] ??? \n\
+      "\n[a-l] radial move \n\
       [arrow] move cursor\n\
       [delete] clear sample\n\
       [tab] last level\n\
@@ -106,6 +175,7 @@ defineMode("14-blank", grid => {
     },
 
     init() {
+      arcRenderer=new ArcRenderer()
       samples = sampleFiles.map(x => new Howl({ src: [x + ".wav", x + ".mp3"] }))
       //setTimeout(tick, playhead.interval)
       timer = setTimeout(tick, playhead.interval)
@@ -114,6 +184,7 @@ defineMode("14-blank", grid => {
     },
     // unload is called when the mode actually unloads
     unload() {
+      arcRenderer=null
       // delete samples array
       samples.length = 0;
     },
@@ -123,7 +194,7 @@ defineMode("14-blank", grid => {
     },
 
     onKey(key) {
-      if (key.key.match(/^[1-9]$/)) {
+      if (key.key.match(/^[a-l]$/)) {
         grid.sequence[grid.cursor.index] = key.key
         //grid.advanceBy(1)
       } else if (key.key == 'Enter') {
@@ -135,8 +206,11 @@ defineMode("14-blank", grid => {
     update(x, y, index) { },
 
     draw(frameCounter) {
+      arcRenderer.run()
       fill(255, 165, 0, orangeAlpha)    // orange playhead
-      drawChar(cursorChar, unitOf(0.75), ...indexToPixelXY(playhead.pos))
+      let playheadPos=indexToPixelXY(playhead.pos)
+      rect(playheadPos[0],playheadPos[1],unitOf(16),unitOfOne())
+      // drawChar(cursorChar, unitOf(0.75), ...indexToPixelXY(playhead.pos))
     },
   }
 })
